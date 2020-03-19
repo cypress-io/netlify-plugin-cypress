c8cb9f11b9bf5d945e3e1f859853ec2b7a4951e9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-03-19 12:31:29 -0400

    chore: refactor postBuild

--- index.js
+++ index.js
@@ -27,6 +27,51 @@ async function runCypressTests (baseUrl, record, spec) {
   })
 }
 
+async function postBuild({ fullPublishFolder, record, spec, failBuild }) {
+  const port = 8080
+  const server = serveFolder(fullPublishFolder, port)
+  debug('local server listening on port %d', port)
+
+  const baseUrl = `http://localhost:${port}`
+
+  const results = await runCypressTests(baseUrl, record, spec)
+
+  await new Promise((resolve, reject) => {
+    server.close(err => {
+      if (err) {
+        return reject(err)
+      }
+      debug('closed local server on port %d', port)
+      resolve()
+    })
+  })
+
+  // seems Cypress TS definition does not have "failures" and "message" properties
+  if (results.failures) {
+    // Cypress failed without even running the tests
+    console.error('Problem running Cypress')
+    console.error(results.message)
+
+    return failBuild('Problem running Cypress', {
+      error: new Error(results.message)
+    })
+  }
+
+  debug('Cypress run results')
+  Object.keys(results).forEach(key => {
+    if (key.startsWith('total')) {
+      debug('%s:', key, results[key])
+    }
+  })
+
+  // results.totalFailed gives total number of failed tests
+  if (results.totalFailed) {
+    return failBuild('Failed Cypress tests', {
+      error: new Error(`${results.totalFailed} test(s) failed`)
+    })
+  }
+}
+
 module.exports = function cypressPlugin (pluginConfig) {
   debugVerbose('cypressPlugin config %o', pluginConfig)
 
@@ -38,29 +83,13 @@ module.exports = function cypressPlugin (pluginConfig) {
       const fullPublishFolder = arg.netlifyConfig.build.publish
       debug('folder to publish is "%s"', fullPublishFolder)
 
-      const port = 8080
-      const server = serveFolder(fullPublishFolder, port)
-      debug('local server listening on port %d', port)
-
       // only if the user wants to record the tests and has set the record key
       // then we should attempt recording
       const record =
         typeof process.env.CYPRESS_RECORD_KEY === 'string' &&
         Boolean(pluginConfig.record)
-      const baseUrl = `http://localhost:${port}`
+
       const spec = pluginConfig.spec
-
-      const results = await runCypressTests(baseUrl, record, spec)
-
-      await new Promise((resolve, reject) => {
-        server.close(err => {
-          if (err) {
-            return reject(err)
-          }
-          debug('closed local server on port %d', port)
-          resolve()
-        })
-      })
 
       const exitWithError = (message, info) => {
         console.error('Exit with error: %s', message)
@@ -68,30 +97,12 @@ module.exports = function cypressPlugin (pluginConfig) {
       }
       const failBuild = arg.utils && arg.utils.build && arg.utils.build.failBuild || exitWithError
 
-      // seems Cypress TS definition does not have "failures" and "message" properties
-      if (results.failures) {
-        // Cypress failed without even running the tests
-        console.error('Problem running Cypress')
-        console.error(results.message)
-
-        return failBuild('Problem running Cypress', {
-          error: new Error(results.message)
-        })
-      }
-
-      debug('Cypress run results')
-      Object.keys(results).forEach(key => {
-        if (key.startsWith('total')) {
-          debug('%s:', key, results[key])
-        }
+      return postBuild({
+        fullPublishFolder,
+        record,
+        spec,
+        failBuild
       })
-
-      // results.totalFailed gives total number of failed tests
-      if (results.totalFailed) {
-        return failBuild('Failed Cypress tests', {
-          error: new Error(`${results.totalFailed} test(s) failed`)
-        })
-      }
     }
   }
 }

d0dc036f83a51e256c3e2e19afee7fb51f0af370
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-09 11:49:09 -0500

    refactor serve

--- src/index.js
+++ src/index.js
@@ -1,45 +1,9 @@
 // @ts-check
-const LocalWebServer = require('local-web-server')
 const debug = require('debug')('netlify-plugin-cypress')
 const debugVerbose = require('debug')('netlify-plugin-cypress:verbose')
-const { ping, getBrowserPath } = require('./utils')
-const fs = require('fs')
+const { ping, getBrowserPath, serveFolder } = require('./utils')
 
 const DEFAULT_BROWSER = 'electron'
-
-function serveFolder(directory, port, spa) {
-  if (typeof spa === 'boolean') {
-    if (spa) {
-      // spa parameter should be the name of the
-      // fallback file in the directory to serve
-      // typically it is "index.html"
-      spa = 'index.html'
-    } else {
-      // do not use fallback mechanism for routing
-      spa = undefined
-    }
-  }
-  debug(
-    'serving local folder %o from working directory %s',
-    {
-      directory,
-      port,
-      spa,
-    },
-    process.cwd(),
-  )
-
-  if (!fs.existsSync(directory)) {
-    throw new Error(`Cannot find folder "${directory}" to serve`)
-  }
-
-  return LocalWebServer.create({
-    // @ts-ignore
-    directory,
-    port,
-    spa,
-  }).server
-}
 
 function startServerMaybe(run, options = {}) {
   const startCommand = options.start
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 // @ts-check
 const puppeteer = require('puppeteer')
 const debug = require('debug')('netlify-plugin-cypress')
+const LocalWebServer = require('local-web-server')
+const fs = require('fs')
 const { ping } = require('./ping')
 
 const getBrowserPath = async () => {
@@ -15,7 +17,50 @@ const getBrowserPath = async () => {
   return info.executablePath
 }
 
+/**
+ * Servers local folder
+ * @see https://github.com/lwsjs/local-web-server
+ * @param {string} directory
+ * @param {number} port
+ * @param {boolean|'index.html'} spa
+ */
+function serveFolder(directory, port, spa) {
+  if (typeof spa === 'boolean') {
+    if (spa) {
+      // spa parameter should be the name of the
+      // fallback file in the directory to serve
+      // typically it is "index.html"
+      spa = 'index.html'
+    } else {
+      // do not use fallback mechanism for routing
+      spa = undefined
+    }
+  }
+  debug(
+    'serving local folder %o from working directory %s',
+    {
+      directory,
+      port,
+      spa,
+    },
+    process.cwd(),
+  )
+
+  if (!fs.existsSync(directory)) {
+    throw new Error(`Cannot find folder "${directory}" to serve`)
+  }
+
+  return LocalWebServer.create({
+    // @ts-ignore
+    directory,
+    port,
+    spa,
+    staticExtensions: 'html',
+  }).server
+}
+
 module.exports = {
   ping,
   getBrowserPath,
+  serveFolder,
 }

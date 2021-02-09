20217651f3ab6a7b90e57035e3b5350886097811
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-09 11:34:53 -0500

    update tests a little

--- tests/html-pages/cypress/integration/spec.js
+++ tests/html-pages/cypress/integration/spec.js
@@ -2,9 +2,11 @@
 describe('html-pages', () => {
   it('loads the index page', () => {
     cy.visit('/')
+    cy.contains('Index page')
   })
 
   it('loads the about page', () => {
     cy.visit('/commands/about')
+    cy.contains('About')
   })
 })
